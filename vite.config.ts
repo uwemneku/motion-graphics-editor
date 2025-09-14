983e69665c50feae0c3829953fb2fc0792f2b2b9
Uwem Israel <iamuwem@gmail.com>
2025-09-14 20:55:02 +0100

    Bug fix: page build error.
    Added relative path for assets to fix github build bug

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react(), tailwindcss()],
+    base: env.BASE_URL,
+  };
 });
